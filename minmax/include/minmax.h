41bde103477077b7653637f48830e6dfe68e9181
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-22 17:35:18 -0300

    questão minmax finalizada

--- minmax/include/minmax.h
+++ minmax/include/minmax.h
@@ -30,28 +30,27 @@ std::pair<Itr, Itr> minmax( Itr first, Itr last, Compare cmp )
     if(first == last) {
         return std::make_pair( first, first );
     } else {
-        Itr ma -> first;
-        Itr me -> first;
-        Itr pos -> first + 1;
-        while (pos != last)
-        {
-            bool x = cmp(const string &ma,  const string &pos);
-            bool y = cmp(const string &pos,  const string &me);
-            if (x == true)
-            {
-                ma -> pos;
+        Itr ma = first;
+        Itr me = first;
+        Itr pos = first + 1;
+        while (pos != last) {
+
+            bool x = cmp(*ma, *pos);
+            bool y = cmp(*pos, *me);
+            
+            if (x == true || *ma == *pos) {
+                ma = pos;
             }
 
-            if (y == true)
-            {
-                me -> pos;
+            if (y == true && *me != *pos) {
+                me = pos;
             }
             
             ++pos;
             
         }
         
-        return std::make_pair( ma, me );
+        return std::make_pair( me, ma );
     }
     
 }
