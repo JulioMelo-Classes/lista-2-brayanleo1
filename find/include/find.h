0a40b5034e541d127e28028f63d80c7287953ed6
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-24 14:54:34 -0300

    Questão find completa

--- find/include/find.h
+++ find/include/find.h
@@ -11,13 +11,28 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range
+ * @tparam Equal verifica a igualdade de dois parâmetros
+ * @tparam T valor constante a ser verificado
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param value Valor constante para ser verificado
+ * @param eq Função que verifica se os valores passados são iguais ou não retornando true ou false
+ * 
+ * @return Um ponteiro que será a primeira posição se existir algo que satisfaça a igualdade ou retornará a posição logo após a última caso não haja
  */
 template<class InputIt, class T, class Equal>
 InputIt find(InputIt first, InputIt last, const T& value, Equal eq)
 {
-    // TODO
-    return first;
+    bool onde = false;
+    auto pos = first;
+    onde = eq(*pos,value);
+    while(pos != last && onde == false) {
+        ++pos;
+        onde = eq(*pos,value);
+    }
+    return pos;
 }
 
 }
