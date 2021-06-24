5c7f8e8c385ee300984d71a4d701923f7a3d616d
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-24 18:25:24 -0300

    Questão all_any_none_of completa

--- all_any_none_of/include/all_any_none_of.h
+++ all_any_none_of/include/all_any_none_of.h
@@ -11,33 +11,82 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range
+ * @tparam UnaryPredicate predicate de um parâmetro que verifica se algo existe na range
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param p Função que verifica se o predicado é verdadeiro ou falso
+ * 
+ * @return boolean que confirma se todos os elementos na range cumprem o predicado p
  */
 template<class InputIt, class UnaryPredicate>
 bool all_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+    bool existe = true;
+    auto pos = first;
+    existe = p(*pos);
+    while(pos != last && existe == true) {
+        ++pos;
+        if(pos != last) {
+            existe = p(*pos);
+        }
+    }
+    return existe;
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range
+ * @tparam UnaryPredicate predicate de um parâmetro que verifica se algo existe na range
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param p Função que verifica se o predicado é verdadeiro ou falso
+ * 
+ * @return boolean que confirma se algum dos elementos na range cumprem o predicado p
  */
 template<class InputIt, class UnaryPredicate>
 bool any_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+    bool existe = false;
+    auto pos = first;
+    existe = p(*pos);
+    while(pos != last && existe == false) {
+        ++pos;
+        if(pos != last) {
+            existe = p(*pos);
+        }
+    }
+    return existe;
 }
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range
+ * @tparam UnaryPredicate predicate de um parâmetro que verifica se algo existe na range
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param p Função que verifica se o predicado é verdadeiro ou falso
+ * 
+ * @return boolean que confirma se nenhum dos elementos na range cumprem o predicado p
  */
 template<class InputIt, class UnaryPredicate>
 bool none_of(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return false;
+    bool existe = false;
+    auto pos = first;
+    existe = p(*pos);
+    while(pos != last && existe == false) {
+        ++pos;
+        if(pos != last) {
+            existe = p(*pos);
+        }
+    }
+    if(existe == true) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 }
