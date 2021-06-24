a217eb1e1bb16d0dafe9529267bd0eb6d90d7e72
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-24 14:47:20 -0300

    Questão find_if completa

--- find_if/include/find_if.h
+++ find_if/include/find_if.h
@@ -11,13 +11,31 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para o range
+ * @tparam UnaryPredicate predicate de um parâmetro que verifica se algo existe na range
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param p Função que verifica se o predicado é verdadeiro ou falso
+ * 
+ * @return Um ponteiro que será a primeira posição se existir algo que satisfaça a condição ou retornará a posição logo após a última
  */
 template<class InputIt, class UnaryPredicate>
 InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
 {
-    // TODO
-    return first;
+    bool onde = false;
+    auto pos = first;
+    onde = p(*pos);
+    while(pos != last && onde == false) {
+        ++pos;
+        onde = p(*pos);
+    }
+    return pos;
+    /*if (onde == true) {
+        return pos;
+    } else {
+        return last;
+    }*/
 }
 
 }
