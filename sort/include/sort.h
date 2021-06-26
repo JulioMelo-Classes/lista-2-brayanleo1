ec328b62c9900a11d070877d01606c6dc9250fe8
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-26 14:55:09 -0300

    Questão sort concluda

--- sort/include/sort.h
+++ sort/include/sort.h
@@ -11,12 +11,31 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam ForwardIt iterator para a range
+ * @tparam Comparison faz uma comparação entre elementos
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param cmp Compara dois elementos recebidos retornando um bool que diz respeito se o primeiro é menor que o segundo
  */
 template<class ForwardIt, class Comparison>
 void sort(ForwardIt first, ForwardIt last, Comparison cmp)
 {
-    // TODO
+    bool maior = false;
+    auto posElem = first;
+    auto posTest = first;
+    while(posElem != last) {
+        while(posTest != last) {
+            maior = cmp(*posTest,*posElem);
+            if(maior == true) {
+                std::swap(*posElem,*posTest);
+            }
+            ++posTest;
+            maior = false;
+        }
+        ++posElem;
+        posTest = posElem;
+    }
 }
 
 }
