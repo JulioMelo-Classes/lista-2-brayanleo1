31993811bfdd4d640d823f604bff76237493da4a
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-26 14:29:24 -0300

    Questão partition completa

--- partition/include/partition.h
+++ partition/include/partition.h
@@ -11,13 +11,38 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam ForwardIt iterator para a range
+ * @tparam UnaryPredicate verifica se o predicate é verdade para o elemento indicado
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param p Função que verifica se o predicate é verdade para o elemento apontado
+ * 
+ * @return Iterator apontando para o elemento logo após o último da range reordenada que satisfaz o predicado
  */
 template<class ForwardIt, class UnaryPredicate>
 ForwardIt partition(ForwardIt first, ForwardIt last, UnaryPredicate p)
 {
-    // TODO
-    return last;
+    bool existe = false;
+    auto posRun = first;
+    auto posLast = first;
+    existe = p(*posRun);
+    if(existe == true) {
+        std::swap(*posLast,*posRun);
+        ++posLast;
+    }
+    while(posRun != last) {
+        ++posRun;
+        if(posRun != last) {
+            existe = p(*posRun);
+            if(existe == true) {
+                std::swap(*posLast,*posRun);
+                ++posLast;
+            }
+        }
+        existe = false;
+    }
+    return posLast;
 }
 
 }
