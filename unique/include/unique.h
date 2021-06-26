6d3aafadbcadaf4ed81f8265b8ba120aeac77c59
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-06-26 14:10:25 -0300

    Questão unique completa

--- unique/include/unique.h
+++ unique/include/unique.h
@@ -11,13 +11,43 @@ using std::sort;
 namespace graal {
 
 /*! 
- * TODO: documentação no estilo doxygen
+ * @tparam InputIt iterator para a range
+ * @tparam Equal verifica se há uma igualdade entre dois elementos
+ * 
+ * @param first Ponteiro para o primeiro elemento da range
+ * @param last Ponteiro para a posição logo após o último elemento da range
+ * @param eq Função que verifica se dois valores em elementos diferentes são iguais
+ * 
+ * @return Iterator apontando para o último elemento da range modificada
  */
 template<class InputIt, class Equal>
 InputIt unique( InputIt first, InputIt last, Equal eq )
 {
-    // TODO
-    return first;
+    bool existe = false;
+    auto posRun = first;
+    auto posLast = first;
+    auto posBef = first;
+    while(posRun != last) {
+        ++posRun;
+        if(posRun != last) {
+            while(posBef != posRun && existe == false) {
+                existe = eq(*posBef,*posRun);
+                ++posBef;
+            }
+            if(existe == true) {
+                auto posAux = posRun;
+                auto posNext = posRun + 1;
+                while(posAux != last) {
+                    *posAux = *posNext;
+                    ++posAux;
+                    ++posNext;
+                }
+            }
+        }
+        posBef = first;
+        existe = false;
+    }
+    return posLast;
 }
 
 }
